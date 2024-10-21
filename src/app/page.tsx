5aae877cea3965ab20c196e9c4772e64fbaa6c02
Domou Brice <domoubrice@gmail.com>
2024-10-21 20:44:25 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
 import Image from "next/image";
-import NavBar from "@/src/components/nav-bar/NavBar";
-import Hero from "@/src/components/hero/Hero";
-import Courses from "@/src/components/courses/Courses";
-import SellingPoint from "@/src/components/selling-point/SellingPoint";
-import Testimonials from "@/src/components/testimonials/Testimonials";
-import JoinPlatForm from "@/src/components/join-platform/JoinPlatform";
-import Footer from "@/src/components/footer/Footer";
 import {NAV_BAR_ITEMS} from "@/src/constants";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-[100px] pb-10">
-        <NavBar />
-        <Hero />
-        <Courses />
-        <SellingPoint />
-        <Testimonials />
-        <JoinPlatForm />
-        <Footer links={NAV_BAR_ITEMS} />
     </div>
   );
 }
--- src/components/nav-bar/NavBarRight.tsx
+++
@@ -1,9 +0,0 @@
-import AuthNavButtons from "@/src/components/nav-bar/AuthNavButtons";
-import SignInRightNav from "@/src/components/nav-bar/SignInRightNav";
-
-const NavBarRight = () => {
-    const authenticated = Math.floor(Math.random() * 2) === 1;
-    return <div>{authenticated ? <AuthNavButtons /> : <SignInRightNav />}</div>;
-};
-
-export default NavBarRight;
